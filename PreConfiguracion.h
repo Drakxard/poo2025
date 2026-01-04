3c0d05d513a49e04e9c1379ba5c49f48df6a476a
Drakxard <drakxard@gmail.com>
2026-01-04 08:48:31 -0300

    Menu Lineal

--- PreConfiguracion.h
+++ PreConfiguracion.h
@@ -7,6 +7,9 @@ System admin("Rafael",1);
 string archiTest = "Test.bin";
 
 ///Carga en vector
+///EL segundo argumento (VerLibros(archiTest,1))es un parche temporal, debe 
+///arreglarse la funcion
+///Así andar para cuando hay o no contenido en el binario correctamente
 vector<Libro> resultadoTest = admin.VerLibros(archiTest,1);	
 
 /// Variable/s simples
--- main.cpp
+++ main.cpp
@@ -10,26 +10,38 @@ int main(){
 	VerContenido(resultadoTest);
 
 {	///Test Agregar libros 
-
+	cout<<endl<<"Preciona para continuar ";
+	cin.get();
+	system("cls");
 	cout<<"LIbros a agregar: ";cin>>Nlibros;
+	if(!Nlibros==0){
 	AgregarLibros(admin,resultadoTest, archiTest, Nlibros);
 	///Recarga del vector desde bin
 	resultadoTest = admin.VerLibros(archiTest);
 	
+	system("cls");
 	ReconocerSolicitud(admin);
 	VerContenido(resultadoTest);
+	cin.ignore();
+	}
 }
 	
 {	///Buscar libro
-	cout<<endl<<"Buscar libro, indica ID: ";cin>>idLibro;
+
+	cin.get();
+	system("cls");
+	cout<<"Buscar libro, indica ID: ";cin>>idLibro;
 	BuscarLibro(admin, idLibro, resultadoTest);
 }	
 	
 	
 {	///Borrar
-	cout<<endl<<"ID a eliminar: ";cin>>idLibro;
+	cin.ignore();
+	cin.get();
+	system("cls");
+	cout<<"ID a eliminar: ";cin>>idLibro;
 	BorrarLibro(admin, resultadoTest, archiTest, idLibro);
-
+		
 	ReconocerSolicitud(admin);
 	VerContenido(resultadoTest);
 }	

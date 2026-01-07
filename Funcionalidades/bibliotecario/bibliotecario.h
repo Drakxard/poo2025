af0da8dec5b4a95303bc365eb296b34f4e5a30b6
Xbox-Koala-Dedicate1 <hilbertfran13@gmail.com>
2026-01-07 10:55:25 -0300

    NEMESIS

--- Funcionalidades/bibliotecario/bibliotecario.h
+++ Funcionalidades/bibliotecario/bibliotecario.h
@@ -7,7 +7,28 @@ class Bibliotecario: public Persona{
  int id;
 public:
 	Bibliotecario(int id, const char *nombre):Persona(id,nombre){};
-
+	// Función auxiliar para calcular diferencia de días desde "hoy" hasta una fecha meta
+	int CalcularDiferenciaDias(int dia, int mes, int anio) {
+		// 1. Obtener fecha actual
+		time_t t = time(0);
+		struct tm * now = localtime(&t);
+		
+		// 2. Configurar fecha de devolución (struct tm)
+		struct tm fechaDevolucion = {0};
+		fechaDevolucion.tm_year = anio - 1900; // Los años en tm cuentan desde 1900
+		fechaDevolucion.tm_mon = mes - 1;      // Los meses van de 0 a 11
+		fechaDevolucion.tm_mday = dia;
+		
+		// 3. Convertir ambas a tiempo en segundos
+		time_t fechaActualSec = mktime(now);
+		time_t fechaDevSec = mktime(&fechaDevolucion);
+		
+		// 4. Calcular diferencia en segundos y convertir a días
+		double seconds = difftime(fechaDevSec, fechaActualSec);
+		int dias = static_cast<int>(seconds / (60 * 60 * 24));
+		
+		return dias > 0 ? dias : 0; // Evitar días negativos si la fecha ya pasó
+	}
 	void GuardarLibros(string nombreArhivo, vector<Libro> &A_Guardar);
     vector<Libro> VerLibros(string nombreArchivo,bool crear=false);
 	void AgregarLibros(int LibrosAgregar,vector<Libro>&resultadoTest);
--- Recursos/librerias/base.h
+++ Recursos/librerias/base.h
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <cstring>
 #include <iomanip>
+#include <ctime>
 using namespace std;
 
 

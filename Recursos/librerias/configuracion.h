81146afeccacdd810f7466b2a559a9d2c5683908
Drakxard <drakxard@gmail.com>
2026-01-07 09:54:27 -0300

    EVOLUTION

--- Recursos/librerias/configuracion.h
+++ Recursos/librerias/configuracion.h
@@ -1,16 +1,14 @@
 #ifndef PRECONFIGURACION_H
 #define PRECONFIGURACION_H
 
-System admin("Rafael",1); 
-
 ///Binario Donde Trabajar
-string archiTest = "Test.bin";
+string libros = "libros.bin";
 
 ///Carga en vector
 ///EL segundo argumento (VerLibros(archiTest,1))es un parche temporal, debe 
 ///arreglarse la funcion
 ///Así andar para cuando hay o no contenido en el binario correctamente
-vector<Libro> resultadoTest = admin.VerLibros(archiTest,1);	
+vector<Libro> vectorLibros = admin.VerLibros(libros,1);	
 
 /// Variable/s simples
 int idLibro;
--- main.cpp
+++ main.cpp
@@ -1,55 +1,102 @@
-#include "ParaMenu.h"
-#include "PreConfiguracion.h"
+#include "Recursos/libreria/base.h"
+#include "Recursos/libreria/configuracion.h"
+#include "Funcionalidades/libro/libro.h"
+#include "Funcionalidades/bibliotecario/bibliotecario.h"
+
 
 int main(){
 
-	///Informar quiï¿½n realiza la acciï¿½n
-	ReconocerSolicitud(admin);
+	
+	///Login
+	string NombreUsuario;
+	int codigo = 1;
+	
+	cout<<"Sistema de gestión Bibliotecario!!!"<<endl<<endl;
+	
+	cout<<"Nombre: "<<NombreUsuario;
+	
+	
+	Bibliotecario admin(NombreUsuario,codigo); 
+	
+	cout<<"Buenas "<< admin.VerNombre()<< " ID: "<< admin.VerID()<<endl;
+	cin.get();
+	system("cls");
+	
+	cout<<"Libros Disponibles: "<<endl;
 	
 	///Contenido Actual
-	VerContenido(resultadoTest);
+	for(Libro &x: vectorLibros)
+		cout
+		<<"Nombre Libro: " 	<<x.VerNombre()
+		<<"  Id: "	<<x.VerID()
+		<<endl; 
+	
+{	///Agregar libros 
+	cout<<"Prueba registrar un nuevo libro!! ( 0 _ 0 )"<<endl;
+	///Terminal			| Entrada
 
-{	///Test Agregar libros 
-	cout<<endl<<"Preciona para continuar ";
 	cin.get();
 	system("cls");
 	cout<<"LIbros a agregar: ";cin>>Nlibros;
+	///Uso del metodo	| Proceso
 	if(!Nlibros==0){
-	AgregarLibros(admin,resultadoTest, archiTest, Nlibros);
-	///Recarga del vector desde bin
-	resultadoTest = admin.VerLibros(archiTest);
+		admin.AgregarLibros(Nlibros,vectorLibros);	
+		admin.GuardarLibros(libros,vectorLibros);
+		vectorLibros = admin.VerLibros(libros);
 	
 	system("cls");
-	ReconocerSolicitud(admin);
-	VerContenido(resultadoTest);
+	///Muestra			| Salida
+	for(Libro &x: vectorLibros)
+		cout
+		<<"Nombre Libro: " 	<<x.VerNombre()
+		<<"  Id: "	<<x.VerID()
+		<<endl; 
 	cin.ignore();
 	}
 }
     
 {	///Buscar libro
-
+	///Terminal			| Entrada
 	cin.get();
 	system("cls");
 	cout<<"Buscar libro, indica ID: ";cin>>idLibro;
-	BuscarLibro(admin, idLibro, resultadoTest);
+	
+	///Buscar Libro		| Proceso
+	vector<Libro>::const_iterator it = admin.BuscarLibro(idLibro,vectorLibros);
+	
+	
+	///Muestra 			| Salida
+	cout<<"Libro: "<<endl
+		<<"Nombre: "<<(*(it)).VerNombre()
+		<<endl 
+		<<"ID: "<<(*(it)).VerID()
+		<<endl;
 	
 	///falta que busque por nombre del libro
 }	
 	
 	
 {	///Borrar
+	///Terminal  		| Entrada
 	cin.ignore();
 	cin.get();
 	system("cls");
 	cout<<"ID a eliminar: ";cin>>idLibro;
-	BorrarLibro(admin, resultadoTest, archiTest, idLibro);
-		
-	ReconocerSolicitud(admin);
-	VerContenido(resultadoTest);
-}	
 	
-
-
+	
+	///Uso del metodo 	| Proceso
+	vector<Libro>::const_iterator itBorrar = admin.BuscarLibro(idLibro,vectorLibros);
+	admin.EliminarLibro(itBorrar,vectorLibros);
+	admin.GuardarLibros(libros,vectorLibros);
+	
+	
+	///Muestra			| Salida
+	for(Libro &x: vectorLibros)
+		cout
+		<<"Nombre Libro: " 	<<x.VerNombre()
+		<<"  Id: "	<<x.VerID()
+		<<endl; 
+}	
 
 	return 0;
 }

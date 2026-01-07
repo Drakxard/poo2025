4bea6aa5582d62af8d8a70b44854cf0bd9303ebf
Drakxard <drakxard@gmail.com>
2026-01-07 17:55:51 -0300

    mejora

---
+++ Funcionalidades/persona.h
@@ -0,0 +1,16 @@
+#include <cstring>
+using namespace std;
+class Persona{
+    int id;
+    char nombre[49]; 
+    public:
+        Persona(int id, const char *nombre){
+                this->id = id;
+                strncpy(this->nombre, nombre, 49);
+                this->nombre[49] = '\0';
+        }
+        int VerID() const;
+        const char *VerNombre() const;
+        void CambiarNombre(const char *NuevoNombre);
+        vector<Libro> VerLibros(string nombreArchivo,bool crear=false);
+};

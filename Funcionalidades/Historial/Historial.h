692a236002e6d8d9fb49353f09e82a5f63e35f14
Drakxard <drakxard@gmail.com>
2026-01-12 16:16:39 -0300

    CAMBIOS2

--- Funcionalidades/Historial/Historial.h
+++ Funcionalidades/Historial/Historial.h
@@ -11,7 +11,7 @@ struct Registro{
 };
 //prestados.bin ->registro de los prestamos
 class Historial{
-    System sistema;
+    System sistema;  
     public:
     bool AgregarNuevoRegistro();
     bool EliminarRegistro(int IdRegistro);

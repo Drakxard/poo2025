67b1e433e9b4bdd41f770edeedd613686ca9f996
Drakxard <drakxard@gmail.com>
2026-01-11 22:40:00 -0300

    5

--- Funcionalidades/bibliotecario/sanciones.cpp
+++ Funcionalidades/bibliotecario/sanciones.cpp
@@ -1,17 +1,13 @@
-#include "../libro/libro.h"
-#include "../alumno/alumno.h"
-#include "../system/system.h"
-#include <cstring>
-#include <iostream>
-
-bool Sancionar(int IdAlumno){
-	///Alumno
-	bool Respuesta = EstadoDisponibilidad(); 
-	if(Respuesta){
+bool Sancionar(int IdAlumno)
+{
+	/// Alumno
+	bool Respuesta = EstadoDisponibilidad();
+	if (Respuesta)
+	{
 		actualizar_disponibilidad(string nombreArchivo, int id);
 	}
 	return true;
-	//Saltar N-pos (id)
-	//Cambiar Su estado a Estado Sancionado
-	///Return true -> sin problemas al sancionar
+	// Saltar N-pos (id)
+	// Cambiar Su estado a Estado Sancionado
+	/// Return true -> sin problemas al sancionar
 }

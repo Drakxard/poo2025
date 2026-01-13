f048de20aee9154f19c83788621e5f6994cf08b9
Aie-s <asegovia338@gmail.com>
2026-01-13 20:15:58 -0300

    nuevo en historial

--- Funcionalidades/Historial/Historial.cpp
+++ Funcionalidades/Historial/Historial.cpp
@@ -8,19 +8,18 @@ template <typename S>
 //Recibir vector entero con N-ID, pueden ser de lo que necesites
 
 bool Historial::Verificar_Existencia( vector<S>&v){//string nombreArchivo){
-        //IdAlumno o IdLibro
-//-> 2, 4, ,8, 248
-
-        // ID: 2, existe registro
-        //----------------------
-        // ID: 4, No existe,,...
-        // ID: 5, No existe,,...
-        // ID: 6, No existe,,...
-        // ID: 7, No existe,,...
-
-        // Buscador->Abre bin, devolveme vector
-        // Recursos/Binarios/Historial/libros.bin
-        // Recursos/Binarios/Historial/alumnos.bin
+	
+	//Buscar si Alumno/Bibliotecario/Libro por ID si existe
+	auto verificacion = find_if(v.begin(), v.end(), [LeerRegistro](const S& a) {
+		return S.VerID() == LeerRegistro;
+	});
+	if(verificacion){
+		Ver_Registro(LeerRegistro, vector<S>&v) //nombreArchivo);
+	}else{
+		cout<<"No existe en el sistema"<<endl;
+		return false;
+	}
+	
 }
 
 
